0335b7aeedba757d97d62f252ff90cf735045533
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2012-09-27 21:00:04 +0100

    Updated Eclipse project files.
    Added JUnit to project dependencies.
    Added a Unit Test class for Thoth class.

---
+++ test/pt/isel/adeetc/cc/thoth/api/client/ThothTests.java
@@ -0,0 +1,33 @@
+package pt.isel.adeetc.cc.thoth.api.client;
+
+import java.text.MessageFormat;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ThothTests extends TestCase {
+
+    private Thoth  thothApiObject;
+
+    private int    thothVersionForTest;
+    private String thothBaseUrlWithVersion;
+
+    @Override
+    @Before
+    protected void setUp() throws Exception {
+        super.setUp();
+        thothApiObject = new Thoth(thothVersionForTest);
+        thothVersionForTest = Thoth.Version.V1;
+        thothBaseUrlWithVersion = MessageFormat.format("http://thoth.cc.e.ipl.pt/api/v{0}",
+                String.valueOf(thothVersionForTest));
+    }
+
+    @Test
+    public void testIfVersionIsCorrect() {
+        assertEquals(thothVersionForTest, thothApiObject.getCurrentWorkingVersion());
+        String thothObjUrl = thothApiObject.getBaseUrl();
+        assertEquals(thothBaseUrlWithVersion, thothObjUrl);
+    }
+}
